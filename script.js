42d36c21695bcddf761b1d11b9349d591ebf7d28
Manojkoli1 <manojkoli108@gmail.com>
2025-07-18 11:41:15 +0530

    changes mades

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ document.querySelectorAll('.nav-link').forEach(link => {
 
 // Initialize Lenis
 
-const lenis = new Lenis();
+const lenis = new Lenis({
+    duration:1.2,
+    easing:(t)=>Math.min(1,1.001 - Math.pow(2, -10*t)),
+    smooth:true
+});
 
-function  raf (time){
+function raf(time){
     lenis.raf(time)
-    requestAnimationFrame(time)
+    requestAnimationFrame(raf)
+
 }
+
+
 requestAnimationFrame(raf)
