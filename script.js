ec77c39f988162e94bbe6cf983fd807cf19effee
Manojkoli1 <manojkoli108@gmail.com>
2025-08-21 15:56:23 +0530

    chnge

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.from("#hero .left",{
+    x:-200,
+    opacity:0,
+    duration:1,
+    stagger:0.2,
+   
+})
+
+gsap.from("#hero .right",{
+    x:700,
+    opacity:0,
+    duration:1,
+    stagger:0.2,
+   
+})
+
+gsap.from("nav",{
+    y:-200,
+    opacity:0,
+    duration:1,
+    stagger:0.2,
+   
+})
