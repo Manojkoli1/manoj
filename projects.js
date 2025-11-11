19dbdd1ecfddab550945d7c38d7e8da8aafb2b16
Manojkoli1 <manojkoli108@gmail.com>
2025-11-11 15:33:54 +0530

    added

---
+++ projects.js
@@ -0,0 +1,28 @@
+let projectsCollection = [
+    {
+        video: "assets/todoEF.mp4",
+        title: "Todo app",
+        technology: "Dotnet Entity Framework Core",
+        description: "Todo app with Entity Framework Core and MVC Architecture"
+    },
+    {
+        video: "assets/todoEF.mp4",
+        title: "Todo app",
+        technology: "Dotnet Entity Framework Core",
+        description: "Todo app with Entity Framework Core and MVC Architecture"
+    }
+
+
+]
+
+const projectsContainer = document.querySelector(".projectsContainer");
+
+projectsCollection.forEach(project => {
+    projectsContainer.innerHTML += ` 
+              <div class="project">
+                   <div class="project1"> <video src="${project.video}" muted autoplay loop></video></div>
+                    <p>Title : Todo app</p>
+                    <p>Technology Used :  ${project.title}</p>
+                    <p>Description : ${project.description} </p>
+                </div>`
+})
