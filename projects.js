92f96226d92f54789027505edc21a1f31feccf73
Manojkoli1 <manojkoli108@gmail.com>
2025-11-13 11:09:21 +0530

    ch

--- projects.js
+++ projects.js
@@ -6,23 +6,12 @@ let projectsCollection = [
         description: "Todo app with Entity Framework Core and MVC Architecture"
     },
     {
-        video: "assets/todoEF.mp4",
-        title: "Todo app",
-        technology: "Dotnet Entity Framework Core",
-        description: "Todo app with Entity Framework Core and MVC Architecture"
+        video: "assets/Rejoice.mp4",
+        title: "Landing Page with Animation",
+        technology: "GSAP, HTML, CSS , Javascript",
+        description: "Landing Page with GSAP Animation"
     },
-     {
-        video: "assets/todoEF.mp4",
-        title: "Todo app",
-        technology: "Dotnet Entity Framework Core",
-        description: "Todo app with Entity Framework Core and MVC Architecture"
-    },
-     {
-        video: "assets/todoEF.mp4",
-        title: "Todo app",
-        technology: "Dotnet Entity Framework Core",
-        description: "Todo app with Entity Framework Core and MVC Architecture"
-    },
+     
     
     
    
@@ -33,7 +22,7 @@ const projectsContainer = document.querySelector(".projectsContainer");
 projectsCollection.forEach(project => {
     projectsContainer.innerHTML += ` 
               <div class="project my-4">
-                   <div class="project1"> <video src="${project.video}" muted autoplay loop></video></div>
+                   <div class="project1"> </div>
                     <p>Title : Todo app</p>
                     <p>Technology Used :  ${project.title}</p>
                     <p>Description : ${project.description} </p>
