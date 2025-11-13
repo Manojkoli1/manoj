4e1f59a824cf16c9117b02cacb42ff199e3c756b
Manojkoli1 <manojkoli108@gmail.com>
2025-11-13 12:01:11 +0530

    added

--- projects.js
+++ projects.js
@@ -1,20 +1,20 @@
 let projectsCollection = [
     {
-        video: "assets/todoEF.mp4",
+        video: "https://www.youtube.com/embed/MRIzz7zEONg",
         title: "Todo app",
         technology: "Dotnet Entity Framework Core",
         description: "Todo app with Entity Framework Core and MVC Architecture"
     },
     {
-        video: "assets/Rejoice.mp4",
+        video: "https://www.youtube.com/embed/63Oxug9R-dw",
         title: "Landing Page with Animation",
         technology: "GSAP, HTML, CSS , Javascript",
         description: "Landing Page with GSAP Animation"
     },
-     
-    
-    
-   
+
+
+
+
 ]
 
 const projectsContainer = document.querySelector(".projectsContainer");
@@ -22,7 +22,19 @@ const projectsContainer = document.querySelector(".projectsContainer");
 projectsCollection.forEach(project => {
     projectsContainer.innerHTML += ` 
               <div class="project my-4">
-                   <div class="project1"> </div>
+                 <div class="project1"><div class="video-container">
+                    <iframe 
+                      width="100%" 
+                       height="400" 
+                       src="${project.video}" 
+                        title="Project Demo" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        allowfullscreen 
+                        loading="lazy">
+                    </iframe>
+                    </div>
+                    </div>
                     <p>Title : Todo app</p>
                     <p>Technology Used :  ${project.title}</p>
                     <p>Description : ${project.description} </p>
