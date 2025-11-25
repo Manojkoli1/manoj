2588c9c3bba45de6a39adfda8276bff662700a59
Manojkoli1 <manojkoli108@gmail.com>
2025-11-25 13:25:16 +0530

    go

--- script.js
+++ script.js
@@ -1,50 +1,60 @@
-gsap.from("#hero .left",{
-    x:-200,
-    opacity:0,
-    duration:1,
-    stagger:0.2,
-   
+gsap.from("#hero .left", {
+    x: -200,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    xPercent: 0,
+    overflow: "hidden"
+
 })
 
-gsap.from("#hero .right",{
-    x:700,
-    opacity:0,
-    duration:1,
-    stagger:0.2,
-   
+gsap.from("#hero .right", {
+    x: 700,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    xPercent: 0,
+    overflow: "hidden"
+
 })
 
-gsap.from("nav",{
-    y:-200,
-    opacity:0,
-    duration:1,
-    stagger:0.2,
-   
+gsap.from("nav", {
+    y: -200,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    xPercent: 0,
+    overflow: "hidden"
+
 })
 
 let portfolio = document.getElementById("portfolio");
 let cursor = document.querySelector(".cursor");
 
-portfolio.addEventListener("mousemove",(e)=>{
-       gsap.to(cursor,{
-        x:e.x,
-        y:e.y,
-        duration:0.5,
-   
-
-       })
+portfolio.addEventListener("mousemove", (e) => {
+    gsap.to(cursor, {
+        x: e.x,
+        y: e.y,
+        duration: 0.5,
+        xPercent: 0,
+        overflow: "hidden"
+    })
 })
 
-portfolio.addEventListener("mouseleave",(e)=>{
-       gsap.to(cursor,{
-      opacity:0
-        
-       })
+portfolio.addEventListener("mouseleave", (e) => {
+    gsap.to(cursor, {
+        opacity: 0,
+        xPercent: 0,
+        overflow: "hidden"
+
+    })
 })
 
-portfolio.addEventListener("mouseenter",(e)=>{
-       gsap.to(cursor,{
-      opacity:1
-        
-       })
+portfolio.addEventListener("mouseenter", (e) => {
+    gsap.to(cursor, {
+        opacity: 1,
+        xPercent: 0,
+        overflow: "hidden"
+
+    })
 })
